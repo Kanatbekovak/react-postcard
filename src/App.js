839b8165a258881c350688aecd5e46c704d1a8ad
Kanatbekovak <kanatbekovakaniet@gmail.com>
2025-12-24 21:31:45 +0600

    new year style

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
+import './App.css';
+import logo from "../src/images/Logo.png";
+import gift from "../src/images/Mancho gift.png";
+import work from "../src/images/Mancho work.png";
+import left from "../src/images/Left branches.png";
+import right from "../src/images/Right branches.png";
 
- import './App.css';
+
 
 function App() {
+  const [wish, setWish] = UseState([]);
   return (
-    <div className="App">
-        <h2 id="goals">Чему я хочу обучиться</h2>
-          <ol>
-            <li>Верстать сайт</li>
-            <li>Пользоваться vs code</li>
-            <li>Хочу научиться учиться</li>
-          </ol>
+    <div className="page-wrapper">
+      <img src={left} className="left-branches" alt="left"/>
+      <img src={right} className="right-branches" alt="right"/>
+      <div className="container">
+        <img src={gift} alt="gift"/>
+        <div className='card'>
+          <img src={logo} alt="gift" />
+          <h1>Happy New Year!</h1>
+          <button>Create a new</button>
+        </div>
+        <img src={work} alt="work"/>
+      </div>
+      <p>New year — new Tech future with<span>Mancho</span></p>
     </div>
   );
 }
