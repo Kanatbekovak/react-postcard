175818bca4195f14860801902a420dfe1ea0594a
Kanatbekovak <kanatbekovakaniet@gmail.com>
2025-12-25 09:54:13 +0600

    js disgn

--- src/App.js
+++ src/App.js
@@ -1,28 +1,56 @@
 import './App.css';
-import logo from "../src/images/Logo.png";
-import gift from "../src/images/Mancho gift.png";
-import work from "../src/images/Mancho work.png";
-import left from "../src/images/Left branches.png";
-import right from "../src/images/Right branches.png";
 
+import logo from "./images/Logo.png";
+import gift from "./images/Mancho gift.png";
+import work from "./images/Mancho work.png";
+import left from "./images/Left branches.png";
+import right from "./images/Right branches.png";
 
+import { useState, useEffect } from 'react';
 
 function App() {
-  const [wish, setWish] = UseState([]);
+  const [wish, setWish] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    fetch('/wishes.json')
+      .then(res => res.json())
+      .then(data => setWish(data));
+  }, []);
+
+  function nextStep() {
+    setCurrentIndex((index) => {
+      if (index === wish.length - 1) {
+        return 0;
+      }
+      return index + 1;
+    });
+  }
+
   return (
     <div className="page-wrapper">
-      <img src={left} className="left-branches" alt="left"/>
-      <img src={right} className="right-branches" alt="right"/>
+      <img src={left} className="left-branches" alt="left" />
+      <img src={right} className="right-branches" alt="right" />
+
       <div className="container">
-        <img src={gift} alt="gift"/>
-        <div className='card'>
-          <img src={logo} alt="gift" />
-          <h1>Happy New Year!</h1>
-          <button>Create a new</button>
+        <img src={gift} alt="gift" />
+
+        <div className="card">
+          <img src={logo} alt="logo" />
+          <h1 className="wish">
+            {wish[currentIndex]?.title}
+          </h1>
+          <button className="btn-create" onClick={nextStep}>
+            Create a new
+          </button>
         </div>
-        <img src={work} alt="work"/>
+
+        <img src={work} alt="work" />
       </div>
-      <p>New year — new Tech future with<span>Mancho</span></p>
+
+      <p>
+        New year — new Tech future with <span>Mancho</span>
+      </p>
     </div>
   );
 }
